4cbe10d559a9f80e20628edf3c6b07521611479f
Eduardo Muñoz Quintás <eduardo@codelab17.com>
2024-05-28 12:14:48 +0200

    Changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8008;
 
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
@@ -16,15 +16,11 @@ function parseEnvList(env) {
   return env.split(',');
 }
 
-// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
-
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
+  requireHeader: [],
   removeHeaders: [
     'cookie',
     'cookie2',
